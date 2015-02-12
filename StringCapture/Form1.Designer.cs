94397af5db8009fb7d0a5f599caee5fc4fc62185
Fixxzer <jakegarner@hotmail.com>
2015-02-12 10:17:54 -0700

    Initial commit.

---
+++ StringCapture/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace StringCapture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRootFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Root Directory";
+            // 
+            // txtRootFolder
+            // 
+            this.txtRootFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRootFolder.Location = new System.Drawing.Point(94, 10);
+            this.txtRootFolder.Name = "txtRootFolder";
+            this.txtRootFolder.Size = new System.Drawing.Size(608, 20);
+            this.txtRootFolder.TabIndex = 1;
+            this.txtRootFolder.Text = "C:\\Dev\\Iris\\Main";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(708, 8);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(40, 23);
+            this.btnBrowse.TabIndex = 2;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.SelectedPath = "C:\\Dev\\Iris\\Main";
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(673, 36);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(75, 23);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.BtnProcess_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 72);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(736, 264);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 349);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtRootFolder);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "String Capture Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRootFolder;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ StringCapture/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringCapture
+{
+    public partial class Form1 : Form
+    {
+        static readonly HashSet<string> Strings = new HashSet<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtRootFolder.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void BtnProcess_Click(object sender, EventArgs e)
+        {
+            new Thread(ProcessFiles).Start();
+        }
+
+        private void ProcessFiles()
+        {
+            if (string.IsNullOrWhiteSpace(txtRootFolder.Text) || string.IsNullOrWhiteSpace(folderBrowserDialog1.SelectedPath))
+                return;
+
+            RecursivelyParseDirectory(folderBrowserDialog1.SelectedPath);
+
+            // Write the results when no more files are found
+            using (StreamWriter fout = new StreamWriter("C:\\Temp\\ParseResults2.txt", false))
+            {
+                foreach (string s in Strings)
+                {
+                    fout.WriteLine(s);
+                }
+            }
+
+            Strings.Clear();
+            MessageBox.Show("Parse Complete");
+        }
+
+        private void RecursivelyParseDirectory(string path)
+        {
+            // Get all the files in the directory
+            IEnumerable<string> files = Directory.EnumerateFiles(path);
+
+            // Output path and current file to listbox
+            foreach (string file in files)
+            {
+                // Hard coded filter
+                if (Path.GetExtension(file) != ".cs")
+                    continue;
+
+                UpdateListBox(file);
+
+                // Add all the parse results to memory, write the results to file after reading is complete for file
+                using (StreamReader fin = new StreamReader(file))
+                {
+                    var text = fin.ReadToEnd();
+
+                    // Only search for strings if the word "SqlCommand" appears somewhere in the file
+                    if (!text.Contains("SqlCommand"))
+                        continue;
+                    
+                    // Slow part, could use some optimizing
+                    bool start = false;
+                    int startIndex = 0;
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (text[i] == '\"')
+                        {
+                            start = !start;
+
+                            // if start is true, we found the first one
+                            if (start)
+                                startIndex = i + 1; // increment by 1 to not include the quote
+                            else // if start is not true, we found the last one, grab the substring between current index and start index
+                            {
+                                string substring = text.Substring(startIndex, i - startIndex);
+                                if (!string.IsNullOrWhiteSpace(substring) &&
+                                    substring.Contains("_") ||
+                                    substring.ToLower().Contains("select") ||
+                                    substring.ToLower().Contains("insert") ||
+                                    substring.ToLower().Contains("update") ||
+                                    substring.ToLower().Contains("delete"))
+                                {
+                                    string[] splitString = substring.Split('.');
+                                    StringBuilder sb = new StringBuilder();
+                                    int stringSplitIndex = 0; 
+                                    for (int j = 0; j < 3; j++)
+                                    {
+                                        if (3 - j <= splitString.Length)
+                                        {
+                                            sb.Append(splitString[stringSplitIndex++]);
+                                        }
+                                        sb.Append("\t");
+                                    }
+                                    sb.Append(file);
+                                    Strings.Add(sb.ToString());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Get the directories, pass directory to recursive method
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs)
+            {
+                RecursivelyParseDirectory(dir);
+            }
+        }
+
+        private void UpdateListBox(string file)
+        {
+            Invoke((MethodInvoker)(() => listBox1.Items.Add(file)));
+            Invoke((MethodInvoker)(() => listBox1.SelectedIndex = listBox1.Items.Count - 1));
+            //if (listBox1.InvokeRequired)
+            //{
+            //    UpdateListBox(file);
+            //}
+            //else
+            //{
+            //    listBox1.Items.Add(file);
+            //    listBox1.SelectedIndex = listBox1.Items.Count - 1;
+            //}
+        }
+    }
+}
